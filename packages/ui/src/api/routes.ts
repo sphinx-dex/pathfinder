12af9dff891e282a0dc8e2c5a17c321ab2690056
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2023-02-03 13:20:21 +0200

    add results component

--- packages/ui/src/api/routes.ts
+++ packages/ui/src/api/routes.ts
@@ -5,11 +5,56 @@ export interface GetRouteProps {
   amountIn: number;
 }
 
+export interface Route {
+  amm: string;
+  type: string;
+  pair: {
+    in: string;
+    out: string; 
+  }
+  proportion: number;
+}
+
+export interface GetRouteResponse {
+  originalPrice: number; // expressed in %
+  priceRoutingImpact: number; // expressed in %
+  slippageReduction: number; // expressed in %
+  routes: Route[];
+  tokensOut: number;
+  originalTokensOut: number;
+}
+
 function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export async function getRoute(props: GetRouteProps) {
+export async function getRoute(props: GetRouteProps): Promise<GetRouteResponse> {
   await sleep(2000);
-  return {};
+  return {
+    originalPrice: -0.2,
+    priceRoutingImpact: -0.02,
+    slippageReduction: -0.33,
+    tokensOut: 1002,
+    originalTokensOut: 889,
+    routes: [
+      {
+        amm: 'Jediswap',
+        type: 'AMM V2',
+        pair: {
+          in: 'ETH',
+          out: 'USDC'
+        },
+        proportion: 0.2
+      },
+      {
+        amm: 'Sphinx',
+        type: 'Orderbook',
+        pair: {
+          in: 'ETH',
+          out: 'USDC'
+        },
+        proportion: 0.8
+      }
+    ]
+  };
 }
